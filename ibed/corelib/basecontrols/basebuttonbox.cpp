f6016b2a9bc9b19e877b1d2ec54b11e45c613a89
Huang Yang <hyjstz@163.com>
2016-06-28 22:07:41 +0800

    使用模板实现basebuttonbox

--- ibed/corelib/basecontrols/basebuttonbox.cpp
+++ ibed/corelib/basecontrols/basebuttonbox.cpp
@@ -1,60 +1,230 @@
-#include <QPushButton>
 #include "basebuttonbox.h"
 #include "ui_basebuttonbox.h"
-#include "boost/foreach.hpp"
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 BaseButtonBox::BaseButtonBox(QWidget *parent) :
     QWidget(parent),
-    ui(new Ui::BaseButtonBox)
+    ui(new Ui::BaseButtonBox),
+    m_orientation(Qt::Horizontal)
+
 {
     ui->setupUi(this);
+    if(m_orientation == Qt::Horizontal)
+        m_layout = new QHBoxLayout;
+    else
+        m_layout = new QVBoxLayout;
+
+    ui->verticalLayout->addLayout(m_layout);
+
 }
+
+BaseButtonBox::BaseButtonBox(Qt::Orientation orientation, QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::BaseButtonBox),
+    m_orientation(orientation)
+{
+    ui->setupUi(this);
+    if(m_orientation == Qt::Horizontal)
+        m_layout = new QHBoxLayout;
+    else
+        m_layout = new QVBoxLayout;
+
+    ui->verticalLayout->addLayout(m_layout);
+
+}
+
 
 BaseButtonBox::~BaseButtonBox()
 {
     delete ui;
 }
 
-void BaseButtonBox::clearButtons()
+void BaseButtonBox::setOrientation(Qt::Orientation orientation)
 {
-    for(QList<QPushButton *>::iterator iter = m_buttons.begin();
+    if(orientation == m_orientation)
+        return ;
+
+    m_orientation = orientation;
+    if(m_layout)
+    {
+        //remove widget
+        for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+            iter != m_buttons.end(); ++iter)
+        {
+            m_layout->removeWidget(iter.value());
+        }
+
+        delete m_layout;
+    }
+
+    if(m_orientation == Qt::Horizontal)
+        m_layout = new QHBoxLayout(this);
+    else
+        m_layout = new QVBoxLayout(this);
+
+    //add widget
+    for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
         iter != m_buttons.end(); ++iter)
     {
-        if((*iter) != NULL)
-            delete (*iter);
+        m_layout->addWidget(iter.value());
+    }
+}
+
+
+
+void BaseButtonBox::clearButtons()
+{
+    for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+        iter != m_buttons.end(); ++iter)
+    {
+        m_layout->removeWidget(iter.value());
+        disconnect(iter.value(), SIGNAL(clicked(bool)), this, SLOT(onButtonClicked()));
+        if(iter.value() != NULL)
+            delete iter.value();
     }
 
     m_buttons.clear();
 }
 
-bool BaseButtonBox::addButton(const QString &name)
+void BaseButtonBox::setSpacing(int spacing)
 {
-    QPushButton *button = new QPushButton(name, this);
-    if(button != NULL)
-    {
-        m_buttons.append(button);
-    }
-    else
-        return false;
-
-    return true;
-
+    m_layout->setSpacing(spacing);
 }
 
-void BaseButtonBox::removeButton(const QString &name)
+
+
+void BaseButtonBox::changeButtonName(int id, const QString &newName)
 {
-    BOOST_FOREACH(QPushButton *button, m_buttons)
+    if(m_buttons.contains(id))
+        m_buttons[id]->setText(newName);
+}
+
+
+void BaseButtonBox::changeButtonName(const QString &origin, const QString &newName)
+{
+    for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+        iter != m_buttons.end(); ++iter)
     {
-        if(button->text() == name)
+        if(iter.value()->text() == origin)
         {
-            m_buttons.removeOne(button);
-            delete button;
+            iter.value()->setText(newName);
+            break;
         }
     }
 }
 
 
-const QList<QPushButton *> BaseButtonBox::buttons() const
+void BaseButtonBox::swapButtons(const QString &src, const QString &dest)
 {
-    return m_buttons;
+    int srcPos = -1, destPos = -1;
+    QAbstractButton *srcButton, *destButton;
+
+    //find button pos
+    for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+        iter != m_buttons.end(); ++iter)
+    {
+        if(iter.value()->text() == src)
+        {
+            srcButton = iter.value();
+            srcPos = iter.key();
+        }
+
+        if(iter.value()->text() == dest)
+        {
+            destButton = iter.value();
+            destPos = iter.key();
+        }
+
+    }
+
+    //swap button
+    if((srcPos != -1) && (destPos != -1))
+    {
+        m_buttons.remove(srcPos);
+        m_buttons.remove(destPos);
+        m_buttons[srcPos] = destButton;
+        m_buttons[destPos] = srcButton;
+    }
+    else
+        return ;
+
+    //refresh button widget
+    for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+        iter != m_buttons.end(); ++iter)
+    {
+        m_layout->removeWidget(iter.value());
+    }
+
+    for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+        iter != m_buttons.end(); ++iter)
+    {
+        m_layout->addWidget(iter.value());
+    }
+
 }
+
+
+void BaseButtonBox::swapButtons(int srcId, int destId)
+{
+    if((m_buttons.contains(srcId)) && (m_buttons.contains(destId)))
+    {
+        QAbstractButton *srcButton = m_buttons[srcId];
+        QAbstractButton *destButton = m_buttons[destId];
+
+        m_buttons.remove(srcId);
+        m_buttons.remove(destId);
+
+        m_buttons[srcId] = destButton;
+        m_buttons[destId] = srcButton;
+
+        //refresh button widget
+        for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+            iter != m_buttons.end(); ++iter)
+        {
+            m_layout->removeWidget(iter.value());
+        }
+
+        for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+            iter != m_buttons.end(); ++iter)
+        {
+            m_layout->addWidget(iter.value());
+        }
+    }
+}
+
+
+void BaseButtonBox::removeButton(const QString &name)
+{
+    for(QHash<int, QAbstractButton *>::iterator iter = m_buttons.begin();
+        iter != m_buttons.end(); ++iter)
+    {
+        if(iter.value()->text() == name)
+        {
+            m_layout->removeWidget(iter.value());
+            disconnect(iter.value(), SIGNAL(clicked(bool)), this, SLOT(onButtonClicked()));
+            delete iter.value();
+            m_buttons.remove(iter.key());
+            break;
+        }
+    }
+
+}
+
+
+void BaseButtonBox::removeButton(int id)
+{
+    if(m_buttons.contains(id))
+    {
+        m_layout->removeWidget(m_buttons[id]);
+        disconnect(m_buttons[id], SIGNAL(clicked(bool)), this, SLOT(onButtonClicked()));
+        delete m_buttons[id];
+        m_buttons.remove(id);
+    }
+}
+
+void BaseButtonBox::onButtonClicked()
+{
+    sender();
+}
+
--- ibed/corelib/basecontrols/basebuttonbox.h
+++ ibed/corelib/basecontrols/basebuttonbox.h
@@ -2,12 +2,14 @@
 #define BASEBUTTONBOX_H
 
 #include <QWidget>
+#include <QHash>
+#include <QAbstractButton>
+#include <QLayout>
 
 namespace Ui {
 class BaseButtonBox;
 }
 
-class QPushButton;
 
 class BaseButtonBox : public QWidget
 {
@@ -15,20 +17,164 @@ class BaseButtonBox : public QWidget
 
 public:
     explicit BaseButtonBox(QWidget *parent = 0);
+    explicit BaseButtonBox(Qt::Orientation orientation, QWidget *parent = 0);
     ~BaseButtonBox();
 
 public:
+    /**
+     * @brief set orientation
+     * @param
+     */
+    void setOrientation(Qt::Orientation orientation);
+
+    /**
+     * @brief add a button, this is a template function,
+     *        T must be inheritd QAbstractButton
+     * @param button name
+     */
+    template <typename T>
     bool addButton(const QString &name);
+
+    /**
+     * @brief change button shown text
+     * @param id: button position
+     * @param newName: button new shown text
+     */
+    void changeButtonName(int id, const QString &newName);
+
+    /**
+     * @brief chang button name
+     * @param origin: origin name
+     * @param newName: new name
+     */
+    void changeButtonName(const QString &origin, const QString &newName);
+
+    /**
+     * @brief swap button position by name
+     * @param first: first button
+     * @param second: second button
+     */
+    void swapButtons(const QString &first, const QString &second);
+
+    /**
+     * @brief swap button position by position
+     * @param firstId: first button position
+     * @param secondId: second button position
+     */
+    void swapButtons(int firstId, int secondId);
+
+    /**
+     * @brief remove button by name
+     * @param name: button name
+     */
     void removeButton(const QString &name);
+
+    /**
+     * @brief remove button by position
+     * @param id: button position based on zero
+     */
+    void removeButton(int id);
+
+    /**
+     * @brief remove all buttons
+     */
     void clearButtons(void);
 
-public:
-    const QList<QPushButton *> buttons(void) const;
+    /**
+     * @brief set button layout spacing
+     * @param spacing: layout spacing
+     */
+    void setSpacing(int spacing);
 
-private:
-    QList<QPushButton *> m_buttons;
+public:
+    /**
+     * @brief return all buttons
+     * @return
+     */
+    template<typename T>
+    const QList<T *> buttons(void) const;
+
+    /**
+     * @brief return button by id
+     * @return
+     */
+    template<typename T>
+    T *button(int id) const;
+
+    /**
+     * @brief return button by name
+     * @return
+     */
+    template<typename T>
+    T *button(const QString &name) const;
+
+signals:
+    void buttonClicked(int id);
+
+private slots:
+    void onButtonClicked(void);
+
 private:
     Ui::BaseButtonBox *ui;
+    Qt::Orientation m_orientation;
+    QLayout *m_layout;
+    QHash<int, QAbstractButton *> m_buttons;
 };
 
-#endif // BASEBUTTONBOX_H
+template <typename T>
+bool BaseButtonBox::addButton(const QString &name)
+{
+    QAbstractButton *button = new T(name, this);
+    if(button != NULL)
+    {
+        button->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        m_buttons[m_buttons.count()] = button;
+        m_layout->addWidget(button);
+        connect(m_buttons[m_buttons.count() - 1], SIGNAL(clicked(bool)), this, SLOT(onButtonClicked()));
+    }
+    else
+        return false;
+
+    return true;
+
+}
+
+template <typename T>
+const QList<T *> BaseButtonBox::buttons() const
+{
+    QList<T *> buttons;
+    for(int i = 0; i < m_buttons.count(); ++i)
+    {
+        buttons.append(dynamic_cast<T *>(m_buttons[i]));
+    }
+
+    return buttons;
+}
+
+template <typename T>
+T *BaseButtonBox::button(int id) const
+{
+    if(m_buttons.contains(id))
+        return dynamic_cast<T *>(m_buttons[id]);
+    else
+        return NULL;
+}
+
+template <typename T>
+T *BaseButtonBox::button(const QString &name) const
+{
+    for(QHash<int, QAbstractButton *>::const_iterator iter = m_buttons.begin();
+        iter != m_buttons.end(); ++iter)
+    {
+        if(iter.value()->text() == name)
+        {
+            return dynamic_cast<T>(iter.value());
+        }
+    }
+
+    return NULL;
+}
+
+
+
+#endif // BaseButtonBox_H
