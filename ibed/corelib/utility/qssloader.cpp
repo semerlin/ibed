582ec16708ee30730e5a90049770de9b26c19887
huang yang <elious.huang@gmail.com>
2017-04-02 21:21:20 +0800

    修改qssloader为静态类

--- ibed/corelib/utility/qssloader.cpp
+++ ibed/corelib/utility/qssloader.cpp
@@ -1,24 +1,38 @@
-/************************************************************
- * Copyright (C), 2016-2017, Intellicare. Co., Ltd.
- * FileName: qssloader.cpp
- * Author: HuangYang
- * Description:     // 模块描述
- * Version:         // 版本信息
- * History:
- *     HuangYang    2016/4/9     1.0     build this moudle
-***********************************************************/
+/*****************************************************************************
+**
+**  Copyright (C) 2016-2017 HuangYang
+**
+**  This file is part of IBED
+**
+**  This program is free software; you can redistribute it and/or modify
+**  it under the terms of the GNU General Public License version 3 as
+**  published by the Free Software Foundation.
+**
+**  You should have received a copy of the GNU General Public License
+**  along with this program. If not, see <http://www.gnu.org/licenses/>.
+**
+**  Unless required by applicable law or agreed to in writing, software
+**  distributed under the License is distributed on an "AS IS" BASIS,
+**  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+**  See the License for the specific language governing permissions and
+**  limitations under the License.
+**
+**  @file     qssloader.cpp
+**  @brief    launch widget
+**  @details  show launch progress and display some messages
+**  @author   huang yang
+**  @email    elious.huang@gmail.com
+**  @version  v1.0.0.0
+**  @license  GNU General Public License (GPL)
+**
+*****************************************************************************/
+
 #include "qssloader.h"
 #include <QStringList>
 #include <QDir>
 #include <QDirIterator>
 #include <QApplication>
 #include <QWidget>
-
-QssLoader::QssLoader() :
-    m_curQss(QString())
-{
-
-}
 
 QStringList QssLoader::findAllQss(const QString &path)
 {
@@ -55,8 +69,6 @@ bool QssLoader::loadQss(const QString &name)
     QByteArray data = file.readAll();
 
     qApp->setStyleSheet(data.constData());
-    m_curQss = name;
-    emit appQssChanged(name);
 
     return true;
 }
@@ -71,7 +83,6 @@ bool QssLoader::loadQss(QWidget *widget, const QString &name)
     QByteArray data = file.readAll();
 
     widget->setStyleSheet(data.constData());
-    emit widgetQssChanged(widget, name);
 
     return true;
 }
---
+++ ibed/modules/thememodule_p.h
@@ -0,0 +1,5 @@
+#ifndef THEMEMODULE_P
+#define THEMEMODULE_P
+
+#endif // THEMEMODULE_P
+
